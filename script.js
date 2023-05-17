4dc4f6a2d0678e2402435a46df679533a7669f0e
tkennelly <theresa.a.kennelly@gmail.com>
2023-05-16 20:27:50 -0400

    add html and css

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log('working')
+
+// let button = document.querySelector("#searchButton")
+
+// const getPokemon = async () => {
+//     const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+//     console.log(pokemon.data)
+//   }
+
+//  // getPokemon()
+
+// button.addEventListener('click', async () => {
+
+//     let pokemonName = document.querySelector("#pokemonName")
+//     let pokemonImage = document.querySelector("#pokemonImage")
+//     //where does this need to be scoped?
+//     let textInput = document.querySelector("#inputBar").value
+//     //Axios call goes here
+//     let response = await axios.get(
+//         `https://pokeapi.co/api/v2/pokemon/${textInput}`
+//       )
+//     //remember to use Async and Await!
+//     //DOM Setters go here
+
+// })
+
+
+
+
